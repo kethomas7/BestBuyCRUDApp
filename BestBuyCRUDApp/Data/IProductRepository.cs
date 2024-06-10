dc12458a1514a76c543f220a2b2b730c2c0ee47f
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-06-10 17:44:18 -0500

    Created app with the products table

---
+++ BestBuyCRUDApp/Data/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using BestBuyCRUDApp.Models;
+
+namespace BestBuyCRUDApp.Data
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        void InsertProduct(Product productToInsert);
+        IEnumerable<Category> GetCategories();
+        Product AssignCategory();
+
+        void DeleteProduct(Product product);
+    }
+}
---
+++ BestBuyCRUDApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Org.BouncyCastle.Bcpg;
+
+namespace BestBuyCRUDApp.Models
+{
+    public class Product
+    {
+
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public bool OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
